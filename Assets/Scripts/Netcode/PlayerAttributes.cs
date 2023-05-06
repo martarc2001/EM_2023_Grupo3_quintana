e93fbb75f50a047b5d3a45616660812ea0e69c55
Marta Rodríguez <martarc2001@hotmail.com>
2023-05-06 12:55:26 +0200

    Player Interfaces

--- Assets/Scripts/Netcode/PlayerAttributes.cs
+++ Assets/Scripts/Netcode/PlayerAttributes.cs
@@ -5,6 +5,7 @@ using UI;
 using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerAttributes : NetworkBehaviour
 {
@@ -24,7 +25,7 @@ public class PlayerAttributes : NetworkBehaviour
         //El owner del objeto avisa al servidor del nombre que ha escogido
         if (IsOwner)
         {
-            string nameInInputText =   GameObject.Find("UI").GetComponent<UIHandler>().playerName;
+            string nameInInputText = GameObject.Find("UI").GetComponent<UIHandler>().playerName;
             ChangeInitialSettingsServerRpc(nameInInputText);
             GetSettingsFromPreviousPlayersServerRpc();
 
@@ -35,10 +36,8 @@ public class PlayerAttributes : NetworkBehaviour
     [ServerRpc]
     void ChangeInitialSettingsServerRpc(string nameInInputText)
     {
-        playerName = nameInInputText;    
-        
-        
-        ChangeInitialSettingsClientRpc(playerName);        
+        playerName = nameInInputText;
+        ChangeInitialSettingsClientRpc(playerName);
     }
 
 
@@ -46,18 +45,49 @@ public class PlayerAttributes : NetworkBehaviour
     void ChangeInitialSettingsClientRpc(string playerName)
     {
         transform.GetChild(0).Find("HUD").Find("Name").GetComponent<TextMeshPro>().text = playerName; //Changing the name on prefab only
+
+    }
+
+    [ClientRpc]
+    void ChangeInitialSettingsClientRpc(string playerName, int thisClientID)
+    {
+        //Changing the name on prefab
+        transform.GetChild(0).Find("HUD").Find("Name").GetComponent<TextMeshPro>().text = playerName; 
         
+
+        //Changing name and interface appearance
+        var otherPlayerInterface = GameObject.Find("Canvas - HUD").transform.GetChild(thisClientID);
+        string otherPlayerSelectedSkin = transform.GetChild(0).gameObject.name.Replace("(Clone)", ""); //When instancing the prefab it shows up as "Huntress(Clone)", removing "(Clone)" for it to be easier to read
+        otherPlayerInterface.gameObject.SetActive(true);
+        otherPlayerInterface.transform.Find("Name").GetComponent<TMPro.TextMeshProUGUI>().text = playerName;
+
+        switch (otherPlayerSelectedSkin)
+        {
+            case "Huntress":
+                otherPlayerInterface.transform.Find("BG").gameObject.GetComponent<Image>().color = Color.green;
+                otherPlayerInterface.transform.Find("Sprite").gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>("Huntress_HUD");
+                break;
+            case "Oni":
+                otherPlayerInterface.transform.Find("BG").gameObject.GetComponent<Image>().color = Color.blue;
+                otherPlayerInterface.transform.Find("Sprite").gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>("Oni_HUD");
+                break;
+            default:
+                otherPlayerInterface.transform.Find("BG").gameObject.GetComponent<Image>().color = Color.red;
+                otherPlayerInterface.transform.Find("Sprite").gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>("AkaiKaze_HUD");
+                break;
+        }
+
     }
 
 
     [ServerRpc]
-    void GetSettingsFromPreviousPlayersServerRpc() 
+    void GetSettingsFromPreviousPlayersServerRpc()
     {
         foreach (NetworkClient client in NetworkManager.Singleton.ConnectedClientsList) //Por cada cliente, coge su respectivo Player Attributes para poder asociar sus variables a animator y nombre
         {
-            
             string name = client.PlayerObject.GetComponentInChildren<PlayerAttributes>().playerName;
-            client.PlayerObject.GetComponentInChildren<PlayerAttributes>().ChangeInitialSettingsClientRpc(name);
+            client.PlayerObject.GetComponentInChildren<PlayerAttributes>().ChangeInitialSettingsClientRpc(name, (int)client.ClientId);
+            Debug.Log("Cliente: " + client.ClientId);
         }
     }
 
